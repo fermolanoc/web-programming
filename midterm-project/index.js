170df9376398cdea8396866d1dbd92cfef8320b5
Fernando Molano <fm87usa@gmail.com>
2021-03-15 08:11:22 -0500

    fix issue with fuction that cause site reloading when submitting button with data without rendering info on DOM

--- midterm-project/index.js
+++ midterm-project/index.js
@@ -1,5 +1,7 @@
 let username = document.querySelector("#username-input");
 let searchButton = document.querySelector("#search-button");
+let user = document.querySelector("#user");
+let cardList = document.querySelector("#card-container");
 
 async function getRepos(username) {
   const baseUrl = await `https://api.github.com/users/${username}/repos`;
@@ -10,9 +12,41 @@ async function getRepos(username) {
     })
     .then((data) => {
       console.log(data);
+
+      user.innerHTML = `Welcome @${username}`;
+
+      let card = document.createElement("div");
+      card.className = "col-12 col-md-6 col-lg-4 mb-4";
+
+      let cardContainer = document.createElement("div");
+      cardContainer.className = "card shadow";
+      card.appendChild(cardContainer);
+
+      let cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+      cardContainer.appendChild(cardBody);
+
+      let projectTitle = document.createElement("h5");
+      projectTitle.className = "card-title text-white fw-bold";
+      projectTitle.innerHTML = data[2].name;
+      cardBody.appendChild(projectTitle);
+
+      let cardBodyDetails = document.createElement("p");
+      cardBodyDetails.className = "card-text fw-light";
+      cardBodyDetails.innerHTML = `Some quick example text to build on the card title and make up
+      the bulk of the card's content.`;
+      cardBody.appendChild(cardBodyDetails);
+
+      let projectLink = document.createElement("button");
+      projectLink.className = "btn btn-danger";
+      projectLink.innerHTML = "View Code";
+      cardBody.appendChild(projectLink);
+
+      cardList.appendChild(card);
     });
 }
 
-searchButton.addEventListener("click", () => {
+searchButton.addEventListener("click", (event) => {
   getRepos(username.value);
+  event.preventDefault();
 });
